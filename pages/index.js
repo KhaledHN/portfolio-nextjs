3ae5736a7d6e6f86048e21642ea81d2941e8ba04
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-07-04 14:12:58 +0300

    Added Skills

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
 import Main from "/components/Main.jsx";
+import Skills from "@/components/Skills.jsx";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Skills />
     </div>
   );
 }
