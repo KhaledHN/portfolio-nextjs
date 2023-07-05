95b87be6e7974217a9f5977cfe33f6ec377b55f5
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-07-06 02:36:55 +0300

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-20 bg-slate-200 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image src="/assets/navLogo.png" alt="" width="100" height="50" />
+        <img src="/assets/navLogo.png" alt="" width="100" height="50" />
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
