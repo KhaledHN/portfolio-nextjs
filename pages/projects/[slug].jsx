6578965fa43aabc94b488b383b8b2138517f2790
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-07-18 04:13:38 +0300

    .

--- pages/projects/[slug].jsx
+++ pages/projects/[slug].jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { RiRadioButtonFill } from "react-icons/ri";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/NavBar/Navbar";
 import projectsData from "../api/Projects.json";
 
 const ProjectPage = () => {
