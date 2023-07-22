956fcc221f2de96d6016c6991d3fe60e5a82210a
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-07-22 14:04:28 +0300

    deleting formspree

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,25 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useForm, ValidationError } from "@formspree/react";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 const Contact = () => {
-
-  const [state, handleSubmit] = useForm("xeqbogvy");
-  if (state.succeeded) {
-      return(
-        <div className="pb-5 px-10 flex justify-center">
-
-          <p className="w-[100px] py-3 rounded-xl text-center bg-gradient-to-r from-[#5752e5] to-[#6f9dff]">  Email sent  </p>
-        </div>
-
-        )
-        
-  }
-
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
@@ -79,69 +65,47 @@ const Contact = () => {
           {/* right */}
           <div className=" col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form onSubmit={handleSubmit} >
+              <form>
                 <div className="flex flex-col py-2">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
-                      id="name"
-                      name="name" 
                     />
                   </div>
-
-                  <ValidationError 
-                   prefix="Named" 
-                   field="name"
-                   errors={state.errors}
-                 />
-
+                </div>
                 <div className=" flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Email</label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
-                    id="email"
-                    type="email" 
-                    name="email"            
-                    />
+                    type="email"
+                  />
                 </div>
-
-                <ValidationError 
-                  prefix="Email" 
-                  field="email"
-                  errors={state.errors}
-                />
 
                 <div className=" flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Message</label>
                   <textarea
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows={10}
-                    id="message"
-                    name="message"
                   ></textarea>
                 </div>
-                <ValidationError 
-                   prefix="Message" 
-                   field="message"
-                   errors={state.errors}
-                 />
-                 <div className="w-full p-4 text-center text-gray-100 rounded-xl mt-4 bg-gradient-to-r from-[#5752e5] to-[#6f9dff] ">
-                  <button type="submit" disabled={state.submitting}>
+                <button className="w-full p-4 text-gray-100 mt-4">
                   Send Message
                 </button>
-                 </div>
-                </div>
-                
               </form>
             </div>
           </div>
         </div>
         <div className="flex justify-center py-12">
           <Link href="/">
+<<<<<<< HEAD
             <div className="rounded-full scale-200 shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
               <HiOutlineChevronDoubleUp className=" h-5 w-5 text-[#5651e1]" scale={30} />
+=======
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5] " size={30} />
+>>>>>>> parent of 59100aa (Making contact section function with formspree)
             </div>
           </Link>
         </div>
